b660a1a691e53b48fef0721b0d6df14497d46289
Pavel <38129764+pavelbrn@users.noreply.github.com>
2020-11-12 14:07:45 +0100

    Update gitUpdate.sh

--- MyScripts/gitUpdate.sh
+++ MyScripts/gitUpdate.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-#commit all files to git master branch
-
+#commit all files into git master branch
 echo "Commit all files in this directory to master branch? y/n "
 read yn
 
+#get user input
 if [ $yn  == "y" ]; then
         echo "Please enter a commit message:  "
         read commit_message
-
+        
         #show git status, add the files and add commit message to hash
         git status
         git add .
-        git commit -m $commit_message
+        git commit -m "$commit_message"
 else
         echo commit cancelled
         exit
 fi
-
 

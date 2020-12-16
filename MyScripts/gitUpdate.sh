8ff03c8fd2f56458b52be37273729601f31012e4
Pavel <38129764+pavelbrn@users.noreply.github.com>
2020-12-16 11:32:41 +0100

    Update gitUpdate.sh

--- MyScripts/gitUpdate.sh
+++ MyScripts/gitUpdate.sh
@@ -1,20 +1,25 @@
-#!/bin/bash
-
-#commit all files into git master branch
-echo "Commit all files in this directory to master branch? y/n "
-read yn
-
-#get user input
-if [ $yn  == "y" ]; then
-        echo "Please enter a commit message:  "
-        read commit_message
-        
-        #show git status, add the files and add commit message to hash
-        git status
-        git add .
-        git commit -m "$commit_message"
-else
-        echo commit cancelled
-        exit
-fi
+  1 #!/bin/bash
+  2 
+  3 #commit all files into git master branch
+  4 echo "Commit all files in this directory to master branch? y/n "
+  5 read yn
+  6 
+  7 #get user input
+  8 if [ $yn  == "y" ] && [ -d .git ] ; then
+  9         echo "Please enter a commit message:  "
+ 10         read commit_message
+ 11         #show git status, add the files and add commit message to hash
+ 12         echo .git
+ 13         git status
+ 14         git add .
+ 15         git commit -m "$commit_message"
+ 16 elif [ ! -d .git  ] ; then
+ 17         echo "There is no git repository in this directory"
+ 18         exit
+ 19         
+ 20 else    
+ 21         echo commit cancelled
+ 22         exit
+ 23 fi      
+ 24 ~     
 
